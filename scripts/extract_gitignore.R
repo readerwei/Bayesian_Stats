6eabb5630fbbf614348fc8545542fb697dd1f439
Wei Zhao <zhaoweiphy@gmail.com>
2020-06-10 13:16:41 -0500

    feat: Update the ignore template

--- scripts/extract_gitignore.R
+++ scripts/extract_gitignore.R
@@ -2,5 +2,7 @@ install.packages('gitignore')
 library(gitignore)
 
 head(gi_available_templates(), 25)
+gi_available_templates()
 gi_fetch_templates("R")
+gi_fetch_templates(c("python","jupyternotebooks"))
 
